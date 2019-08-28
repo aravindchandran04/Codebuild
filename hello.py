491d867104f6120c039a6fb1a9ac6b4494d9971f
Aravind Chandran <39665589+aravindchandran04@users.noreply.github.com>
2019-08-28 16:03:34 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import boto3
-ec2_client = boto3.client('ec2',region_name='us-east-1	')
+ec2_client = boto3.client('ec2',region_name='us-east-1')
 response = ec2_client.describe_instances()
 print(response)
 
