1b2a5745da833867714e7704e40affc129aa9a05
Aravind Chandran <39665589+aravindchandran04@users.noreply.github.com>
2019-08-28 16:11:23 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import boto3
-import requests
-ec2_client = boto3.client('ec2',aws_access_key_id=AKIAUMZO4WIRA5CVL6UU,aws_secret_access_key=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)
+#import requests
+ec2_client = boto3.client('ec2',aws_access_key_id='AKIAUMZO4WIRA5CVL6UU',aws_secret_access_key='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')
 
 
 response = ec2_client.describe_instances()
